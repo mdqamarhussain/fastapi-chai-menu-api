579836a64b92a65ee7b1bd2272e72f41a29a3feb
Md Qamar Hussain <qamar.hussain371@gmail.com>
2026-09-19 20:09:58 +0530

    chai-menu-api

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Query, HTTPException
+from models import MenuItem, MenuResponse
+from data import menu_items
+app = FastAPI(
+    title="Chai Point menu API",
+    description="Read only menu API for Kiosk displays and mobile app"
+)
+
+@app.get("/")
+def root():
+    return {
+        "message" : "Welcome to chai point Menu API"
+    }
+    
+# /menu -> path
+# /menu?category=chai -> query
+
+@app.get("/menu", response_model=MenuResponse)
+def get_menu(category: str | None = Query(None, description="Filter by chai, snack or combo")):
+    if category:
+        """
+        filtered = []
+        for item in menu_items:
+            if item["category"] == category.lower():
+                 filtered.append(item)
+
+        """
+        # the below code is equivalent to the above commented code, but more pythonic
+        filtered = [item for item in menu_items if item["category"] == category.lower()]
+        if not filtered:
+            raise HTTPException(status_code=404, detail=f"No item found in category: {category}")
+        return MenuResponse(count=len(filtered), items=filtered)
+    
+    return MenuResponse(count=len(menu_items), items=menu_items)
+
+@app.get("/menu/{item_id}", response_model=MenuItem)
+def get_item(item_id: int):
+    for item in menu_items:
+        if item["id"] ==  item_id:
+            return item
+        
+    raise HTTPException(status_code=404, detail=f"Menu item with id {item_id} not found")
